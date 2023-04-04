e80f9eac487ccf3529b3d9b20194417fee5c2022
Chrissypike <you@example.com>
2023-04-04 13:38:02 -0500

    mongodb connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/social-network', {
+mongoose.connect('mongodb://localhost:27017/module18', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
